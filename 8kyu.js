f3dd656e290efdd6be9da2c642bb2f3c7af28ab6
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2020-09-25 23:23:36 -0600

    fundamental practice for speed without running code before testing

--- 8kyu.js
+++ 8kyu.js
@@ -83,3 +83,18 @@ function getPlanetName(id){
 }
 
 
+
+
+/*You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.*/
+
+
+function positiveSum(arr) {
+return arr.reduce((sum, curr) => {
+  curr > 0 ? sum += curr : sum += 0
+  return sum
+}, 0)
+}
